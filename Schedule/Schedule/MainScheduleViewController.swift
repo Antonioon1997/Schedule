0e5d7c290756734071f47afb671ab27dd9ea78a0
Antonioon <good-price0@mail.ru>
2022-09-11 17:04:17 +0300

    EmptyProject

---
+++ Schedule/Schedule/MainScheduleViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by Антон Онищенко on 11.09.2022.
+//
+
+import UIKit
+
+class MainScheduleViewController: UIViewController {
+    
+    var mainTableView: UITableView?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
